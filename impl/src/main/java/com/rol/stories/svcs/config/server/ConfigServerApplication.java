12d77a4f1075d52035e69d969f9237ceab73445d
guillermopriego <guillermopriego@telefonica.net>
2017-05-12 19:09:37 +0200

    dependency management

--- impl/src/main/java/com/rol/stories/svcs/config/server/ConfigServerApplication.java
+++ impl/src/main/java/com/rol/stories/svcs/config/server/ConfigServerApplication.java
@@ -3,7 +3,6 @@ package com.rol.stories.svcs.config.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.ComponentScan;
 
 @EnableConfigServer
 @SpringBootApplication
